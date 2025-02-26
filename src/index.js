cab6918b7595b92fa81d37911e35cb41fb1a40e1
Popeye <63921078+popeyeight@users.noreply.github.com>
2025-02-26 16:17:19 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const tabtitle = document.querySelector("title");
+tabtitle.style.textTransform = "uppercase";
+
+const popetitle = document.createElement("div");
+popetitle.textContext = "popeye";
+popetitle.style.fontSize = "2.6vmin";
+popetitle.style.color = "#bbddceff";
+popetitle.style.height = "8vmin";
+popetitle.style.textAlign = "right";
+popetitle.style.letterSpacing = "0.8vmin";
+popetitle.style.textTransform = "uppercase";
+body.appendChild(popetitle);
+
+body.style.backgroundColor = "#26119287";
