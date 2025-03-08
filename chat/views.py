582403a0992bf50032e6324e96943d2df87d7070
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-03-08 11:33:58 +0530

    Delete chat directory

--- chat/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def chat_view(request):
-    return render(request, "chat/chat.html")
-
