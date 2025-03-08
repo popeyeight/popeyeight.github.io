8c4881eb9c483d2d0b4ee9dc537744a6219825cf
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-03-08 01:21:14 -0500

    Update freeze.py

--- freeze.py
+++ freeze.py
@@ -1,7 +1,11 @@
 from flask_frozen import Freezer
-from nm import app  # Replace with your Flask app's import
+from app import app  # Replace with your Flask app's import
 
 freezer = Freezer(app)
 
 if __name__ == '__main__':
-    freezer.freeze()
+    try:
+        freezer.freeze()
+        print("Freezing successful.")
+    except Exception as e:
+        print(f"Error during freezing: {e}")
